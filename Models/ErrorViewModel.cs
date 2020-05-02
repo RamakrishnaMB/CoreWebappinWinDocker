c2c18c7bee4964da5e3e55d7e98c6033e1d85f40
Ramakrishna <ramakrishnamb@gmail.com>
2020-05-02 17:29:02 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreWebappinWinDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
